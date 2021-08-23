99462ce7d68b0c56147d525a92dcf7697f2e6a14
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-23 18:44:35 +0200

    Topo get info

--- lib/tasks/scrap.rb
+++ lib/tasks/scrap.rb
@@ -10,4 +10,9 @@ rivers_url = []
 
 html_doc.search('.nom_site a').each { |el| rivers_url << "https://www.eauxvives.org#{el['href']}" }
 
-puts rivers_url
+i = 0
+10.times do
+  html_topo = URI.open(rivers_url[i]).read
+  html_doc = Nokogiri::HTML(html_topo)
+  i += 1
+end
