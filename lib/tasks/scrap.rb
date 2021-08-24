d563d5a616cb213da6d7e4cba9712996b4582dcc
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-24 18:35:20 +0200

    Add to connect to db

--- lib/tasks/scrap.rb
+++ lib/tasks/scrap.rb
@@ -25,13 +25,12 @@ html_doc.search('.liste_sites tr').each do |el|
   end
 end
 
-i = 1
+i = 0
 e = 0
-2.times do
-  # begin
+20.times do
+  begin
     html_topo = URI.open("https://www.eauxvives.org#{rivers_url[i]}").read
     html_doc = Nokogiri::HTML(html_topo)
-    # Get value of name length and level
     url = rivers_url[i]
     river_name = url[/voir\/(.*$)/, 1]
 
@@ -44,21 +43,23 @@ e = 0
         html_doc.search('#points_acces div').each do |point|
           rivers[river_name][name.to_sym] << point.xpath('text()').to_s.strip
         end
-        presentation = html_doc.search('.rubrique')[1]
-        logistique = html_doc.search('.rubrique')[-2]
-        ap logistique
+        presentation = html_doc.search('.rubrique')[-10]
+        rivers[river_name][name.to_sym] << presentation.search('div:nth-child(2)').xpath('text()').to_s.strip
+        logistique = html_doc.search('.rubrique')[-7]
+        rivers[river_name][name.to_sym] << logistique.search('div:nth-child(2)').xpath('text()').to_s.strip
+        eau = html_doc.search('.rubrique')[-2]
+        rivers[river_name][name.to_sym] << eau.search('div:nth-child(2)').xpath('text()').to_s.strip
       end
     end
-    # ap rivers[river_name]
-    # Get information about topos
     puts "#{rivers_url[i]} - Saved!".green
+  rescue
+    puts "Can't get informations for #{rivers_url[i]}".red
+    e += 1
+  ensure
     i += 1
-  # rescue
-  #   puts "Can't get informations for #{rivers_url[i]}".red
-  #   e += 1
-  # ensure
-  #   i += 1
-  # end
+  end
 end
 
 puts e.zero? ? "#{e} - River not found".green : "#{e} - River not found".red
+
+ap rivers
