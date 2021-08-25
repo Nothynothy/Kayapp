52b93937a3fadf7d9e93d2b659c3c7bf4e20ca90
Simon Granger <nothem6@gmail.com>
2021-08-25 18:21:37 +0200

    finish cards

--- app/controllers/topos_controller.rb
+++ app/controllers/topos_controller.rb
@@ -2,11 +2,9 @@ class ToposController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def show
-
   end
 
   def index
     @topos = Topo.all
   end
-
 end
--- app/models/topo.rb
+++ app/models/topo.rb
@@ -4,7 +4,11 @@ class Topo < ApplicationRecord
   has_many :comments
   has_many :topo_details
 
-  def alert?
-    [true, false].sample
+  def has_severity_2_alert?
+    comments.where(category: "alert", severity: 2, active: true).any?
+  end
+
+  def has_alert?
+    comments.where(category: "alert").any?
   end
 end
