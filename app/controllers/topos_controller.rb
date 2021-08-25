dc36c0501b505d1e028026fa9992832c5497dd03
Anne-Charlotte Morizot <annecharlotte.morizot@gmail.com>
2021-08-25 13:16:57 +0200

    struture CSS topo-show

--- app/controllers/topos_controller.rb
+++ app/controllers/topos_controller.rb
@@ -1,8 +1,8 @@
 class ToposController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def show
-
+    @topo = Topo.find(params[:id])
   end
 
   def index
