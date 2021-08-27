d672d59084d9a8ba61a73c478da5b7b05e0f928e
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-27 13:15:45 +0200

    Include filters

--- app/models/topo.rb
+++ app/models/topo.rb
@@ -10,7 +10,8 @@ class Topo < ApplicationRecord
   pg_search_scope :search_by_topo_and_river,
     against: [ :name ],
     associated_against: {
-      river: [ :name, :country, :region, :department ]
+      river: [ :name, :country, :region, :department ],
+      levels: [ :difficulty ]
     },
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
