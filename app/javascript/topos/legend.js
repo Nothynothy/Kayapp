709e2660dc326a050d65f368c02c9bd2fcfc20c1
Nathan Berthier <nathanberthier@outlook.fr>
2021-08-31 22:03:49 +0200

    add local storage

--- app/javascript/topos/legend.js
+++ app/javascript/topos/legend.js
@@ -1,9 +1,12 @@
 const closeLegend = () => {
   const closeBtn = document.querySelector('.cross');
   const divLegend = document.querySelector('.search-legend');
+  const legendStatus = localStorage.getItem('legendAlert');
+  if (legendStatus == 'hide'){ divLegend.style.display = 'none' };
   if (closeBtn !== null){
     closeBtn.addEventListener('click', () => {
       divLegend.style.display = 'none';
+      localStorage.setItem('legendAlert', 'hide');
     })
   }
 }
