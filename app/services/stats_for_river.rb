23f7cd1eb6be96e4d9dfd685b8df28a1f6c04a0a
Simon Granger <nothem6@gmail.com>
2021-08-31 12:43:48 +0200

    cleaning

--- app/services/stats_for_river.rb
+++ app/services/stats_for_river.rb
@@ -3,24 +3,6 @@ class StatsForRiver < ApplicationService
     @river = river
   end
 
-
-
-#   def call
-#     sites = ApiHubeauInfoSite.call(@river.name)
-#     sites.map! do |site|
-#       {
-#         name: site[:name],
-#         data: ApiHubeauDataSite.call(site[:code]),
-#       }
-#     end
-#     sites.reject do |site|
-#       site[:data].empty?
-#     end
-#   end
-# end
-
-
-
   def call
     sites = ApiHubeauInfoSite.call(@river.name)
     sites.map! do |site|
@@ -29,14 +11,12 @@ class StatsForRiver < ApplicationService
         data: ApiHubeauDataSite.call(site[:code]),
       }
     end
-
     sites.each do |site|
       site[:data].each do |data|
         data[:date].gsub!(/[TZ]/, 'T' => '_', 'Z' => '')
         data[:level] = data[:level]/1000
       end
     end
-
     sites.reject do |site|
       site[:data].empty?
     end
