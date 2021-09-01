bcfa5f184043d0724bfcaaf69c321012e403f8f1
Simon Granger <nothem6@gmail.com>
2021-09-01 15:49:21 +0200

    fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,9 @@
 class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.where(user_id: current_user.id) # => array d'instance de fav
-
-      @topos_fav = @favorites.map(&:topo) # => array d'instance de topo
-      @rivers_fav = @topos_fav.map(&:river) # => array d'instance de river
-      @topos_fav_river_name = @rivers_fav.map(&:name)
-
+    @topos_fav = @favorites.map(&:topo) # => array d'instance de topo
+    @rivers_fav = @topos_fav.map(&:river) # => array d'instance de river
+    @topos_fav_river_name = @rivers_fav.map(&:name)
   end
 
   def delete
