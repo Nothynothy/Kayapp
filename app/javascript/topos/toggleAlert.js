963de22afa591d0aa2c73da323a35e5182b0eeee
Nathan Berthier <nathanberthier@outlook.fr>
2021-09-01 12:01:33 +0200

    Fix margin

--- app/javascript/topos/toggleAlert.js
+++ app/javascript/topos/toggleAlert.js
@@ -2,8 +2,15 @@ const toggleAlert = () => {
   const chevron = document.querySelector('.fa-chevron-down');
   const alertBanner = document.querySelector('.alert-banner');
   const graph = document.querySelector('#water-info')
+  const topoCard = document.querySelector('.topo')
+
+  console.log(topoCard);
+
   graph.style.display = "flex";
   if (chevron !== null){
+
+    topoCard.style.margin = "130px 0px 0px 0px";
+
     chevron.addEventListener('click', () => {
       alertBanner.classList.toggle("alert-display");
       if (graph.style.display === "flex") {
@@ -12,6 +19,8 @@ const toggleAlert = () => {
         graph.style.display = "flex";
       }
     })
+  } else {
+    topoCard.style.margin= "80px 0px 0px 0px";
   }
 }
 
